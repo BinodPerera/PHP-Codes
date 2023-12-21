a918bb3ccbc6c4e5968c2fab285b2943f7def2d7
BinodPerera <yasindubinod7@gmail.com>
2023-12-21 08:48:21 +0530

    push

--- mutiple_forms/form.php
+++ mutiple_forms/form.php
@@ -3,11 +3,31 @@
         <title>Multiple Forms</title>
     </head>
     <body>
-        <form action="form.php">
-            <h4>Form 1</h4>
-            <input type="text" name="f_name" placeholder="f_name">
-            <input type="text" name="l_name" placeholder="l_name">
-            <input type="submit" value="submit" name="form1">
-        </form>
+    <form method="post" action="form.php">
+        <!-- Form 1 -->
+        <input type="text" name="form1_input1">
+        <input type="submit" name="form1_submit" value="Submit Form 1">
+    </form>
+
+    <form method="post" action="form.php">
+        <!-- Form 2 -->
+        <input type="text" name="form2_input1">
+        <input type="submit" name="form2_submit" value="Submit Form 2">
+    </form>
+
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST['form1_submit'])) {
+                $form1_input1 = $_POST['form1_input1'];
+                echo "<script>alert('form 1 submitted.');</script>";
+
+            } elseif (isset($_POST['form2_submit'])) {
+                $form2_input1 = $_POST['form2_input1'];
+                echo "<script>alert('form 2 submitted.');</script>";
+            }
+        }
+    ?>
+
+
     </body>
 </html>
