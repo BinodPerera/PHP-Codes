d1b72ac37d203feef633b6594d084b762d727ede
BinodPerera <yasindubinod7@gmail.com>
2023-12-21 08:41:11 +0530

    push

---
+++ mutiple_forms/form.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Multiple Forms</title>
+    </head>
+    <body>
+        <form action="form.php">
+            <h4>Form 1</h4>
+            <input type="text" name="f_name" placeholder="f_name">
+            <input type="text" name="l_name" placeholder="l_name">
+            <input type="submit" value="submit" name="form1">
+        </form>
+    </body>
+</html>
