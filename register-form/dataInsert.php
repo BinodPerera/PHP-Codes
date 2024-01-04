d27670109f339bc876880dcbf7eaa38b66535a35
Binod-Perera <108719707+BinodPerera@users.noreply.github.com>
2024-01-04 09:13:49 +0530

    Update dataInsert.php

--- register-form/dataInsert.php
+++ register-form/dataInsert.php
@@ -1,20 +1,20 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "database");
+$conn = new mysqli("localhost", "root", "", "database");    // database connection
 
-if($conn->connect_error){
+if($conn->connect_error){    //checking database successfully connected or not.
     die("connection failed! " . $conn->connect_error);
 }
 
-$name = strtoupper($_REQUEST['f_name'] ." ". $_REQUEST['l_name']);
-$email = $_REQUEST['email'];
-$pwd = $_REQUEST['pwd'];
+$name = strtoupper($_REQUEST['f_name'] ." ". $_REQUEST['l_name']);  //strtoupper functions is used for make strings uppercase letters
+$email = $_REQUEST['email'];    //getting email value from html form and assign that value into php variable name '$email'
+$pwd = $_REQUEST['pwd'];    //getting pwd value from html form and assign that value into php variable name '$pwd'
 
 
-$insertData = "INSERT INTO user(name, email, password, roll) VALUES( '$name', '$email', '$pwd', 0)";
+$insertData = "INSERT INTO user(name, email, password, roll) VALUES( '$name', '$email', '$pwd', 0)";    // data insert into database using mysqli query
 
-if(mysqli_query($conn, $insertData)){
-    echo "data inserted!";
+if(mysqli_query($conn, $insertData)){    // checking that mySQL query status using if condition
+    echo "data inserted!";    //if data is successfully inserted this line will be displayed.
 }
 
 ?>
