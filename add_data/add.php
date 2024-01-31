c170d7a6a217d65ee2d6d6fde75d8c80e15661a1
BinodPerera <yasindubinod7@gmail.com>
2024-01-31 20:53:32 +0530

    insert data

--- add_data/add.php
+++ add_data/add.php
@@ -1,5 +1,35 @@
-<?php
+<html>
+    <head>
+        <title>Insert Data into database</title>
 
-$sql = "insert into package()"
+        <?php
 
-?>
+        $conn = new mysqli("localhost","root","","database");
+
+        if($conn->connect_error){
+            die ("Connection failed!".$conn->connect_error);
+        }
+
+        ?>
+    </head>
+    <body>
+        <form action="add.php">
+            <table>
+                <tr>
+                    <td colspan="2"><h3>User Adding form</h3></td>
+                </tr>
+                <tr>
+                    <td><label for="name">Enter name:</label></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td><label for="name">Enter Password:</label></td>
+                    <td><input type="password" name="password"></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" value="Add user"></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
