d0bf42b5bc9b594e71a13b664b652f00032a84dd
BinodPerera <yasindubinod7@gmail.com>
2024-02-01 21:26:11 +0530

    push

--- add_data/add.php
+++ add_data/add.php
@@ -13,7 +13,7 @@
         ?>
     </head>
     <body>
-        <form action="add.php">
+        <form action="add.php" method="post">
             <table>
                 <tr>
                     <td colspan="2"><h3>User Adding form</h3></td>
@@ -27,9 +27,25 @@
                     <td><input type="password" name="password"></td>
                 </tr>
                 <tr>
-                    <td colspan="2"><input type="submit" value="Add user"></td>
+                    <td><label for="name">Enter confirm Password:</label></td>
+                    <td><input type="password" name="c_password"></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" name="submitButton" value="Add user"></td>
                 </tr>
             </table>
         </form>
+
+        <?php
+
+        if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submitButton"])){
+            $name = $_REQUEST['name'];
+            $password = $_REQUEST['password'];
+            $c_password = $_REQUEST['c_password'];
+
+            $isert_query = "INSERT INTO database() VALUESS($)"
+        }
+
+        ?>
     </body>
 </html>
