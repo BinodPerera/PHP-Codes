1d2c16b9579b5dbf0cf9c44830bd9a61fa69fd9e
BinodPerera <yasindubinod7@gmail.com>
2024-03-10 10:36:39 +0530

    push

--- Move_file/move-file.php
+++ Move_file/move-file.php
@@ -1,3 +1,15 @@
+<?php 
+    date_default_timezone_set('Asia/Colombo');
+    // Get the current date and time in Sri Lanka
+    $currentDateTime = new DateTime();
+
+    // Format the date and time as desired
+    $currentTimeInSriLanka = $currentDateTime->format('Y-m-d H:i:s');
+
+    // Output the current time in Sri Lanka
+    echo "Current time in Sri Lanka: " . $currentDateTime;
+?>
+
 <form method="post" enctype="multipart/form-data">
     <label for="file">choose file</label>
     <input type="file" name="image">
